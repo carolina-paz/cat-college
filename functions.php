0ddbee0fa3acbca9f678f27f903f3added7037b7
Carolina Ruz <kro.ruz@gmail.com>
2024-02-18 14:17:51 -0300

    tailwind activated

--- functions.php
+++ functions.php
@@ -5,4 +5,10 @@ function college_files() {
 }
 add_action('wp_enqueue_scripts', 'college_files');
 
+function enqueue_styles() {
+    wp_enqueue_style('tailwind', get_template_directory_uri() . '/output.css');
+}
+add_action('wp_enqueue_scripts', 'enqueue_styles');
+
+
 ?>
